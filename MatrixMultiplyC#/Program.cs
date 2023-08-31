935e50a4955256e52a7323080e12e595549411c3
Dan Lopez <dan.g.lopez@outlook.com>
2023-08-31 20:55:53 +1000

    :tada: Matrix Multiply Example - Parallel - Array Tranpose

--- MatrixMultiplyC#/Program.cs
+++ MatrixMultiplyC#/Program.cs
@@ -8,36 +8,36 @@ internal class Program
     const int N = 300;
     const int RUNS = 100;
     const int Seed = 42;
-    
+
 
     private static void Main(string[] args)
     {
         Stopwatch stopwatch = new Stopwatch();
 
-        double[,] A = new double[N,N];
+        double[,] A = new double[N, N];
         double[,] B = new double[N, N];
         double[,] C = new double[N, N];
 
-        generateArray( A,  B);
+        generateArray(A, B);
         stopwatch.Start();
-        
+
         // Matrix multiply by the amount of runs.
         for (int i = 0; i < RUNS; i++)
         {
-            multiplyArrays( A,  B,  C);            
+            multiplyArrays(A, B, C);
         }
-        
+
         stopwatch.Stop();
 
         TimeSpan elapsed = stopwatch.Elapsed;
-        Console.WriteLine($"Elapsed time: {elapsed.TotalSeconds} s.\nWith {RUNS} runs of a {N}x{N} matrix.");
+        Console.WriteLine($"Sequential Elapsed time: {elapsed.TotalSeconds} s.\nWith {RUNS} runs of a {N}x{N} matrix.");
         Console.WriteLine("----------------------------------------------");
-        displayArrayLastNums(C,5);
+        displayArrayLastNums(C, 5);
 
 
         //Reset and Clear
-        Console.WriteLine("\n\n");        
-        stopwatch.Reset();        
+        Console.WriteLine("\n\n");
+        stopwatch.Reset();
         C = new double[N, N];
 
 
@@ -48,11 +48,50 @@ internal class Program
             multiplyParrallelArrays(A, B, C);
         }
         stopwatch.Stop();
-        
+
         elapsed = stopwatch.Elapsed;
         Console.WriteLine($"Parrallel Elapsed time: {elapsed.TotalSeconds} s.\nWith {RUNS} runs of a {N}x{N} matrix.");
         Console.WriteLine("----------------------------------------------");
         displayArrayLastNums(C, 5);
+
+        stopwatch.Reset();
+        C = new double[N, N];
+        generateArrayT(A, B);
+
+        stopwatch.Start();
+        // Parrallel Matrix multiply by the amount of runs.
+        for (int i = 0; i < RUNS; i++)
+        {
+            multiplyArraysTrans(A, B, C);
+        }
+        stopwatch.Stop();
+
+        elapsed = stopwatch.Elapsed;
+        Console.WriteLine("\n\n");
+        Console.WriteLine($"Sequential Transposed Elapsed time: {elapsed.TotalSeconds} s.\nWith {RUNS} runs of a {N}x{N} matrix.");
+        Console.WriteLine("----------------------------------------------");
+        displayArrayLastNums(C, 5);
+
+
+
+
+        stopwatch.Reset();
+        C = new double[N, N];        
+
+        stopwatch.Start();
+        // Parrallel Matrix multiply by the amount of runs.
+        for (int i = 0; i < RUNS; i++)
+        {
+            multiplyParallelArraysTrans(A, B, C);
+        }
+        stopwatch.Stop();
+
+        elapsed = stopwatch.Elapsed;
+        Console.WriteLine("\n\n");
+        Console.WriteLine($"Parallel Transposed Elapsed time: {elapsed.TotalSeconds} s.\nWith {RUNS} runs of a {N}x{N} matrix.");
+        Console.WriteLine("----------------------------------------------");
+        displayArrayLastNums(C, 5);
+
     }
 
     public static void multiplyParrallelArrays(double[,] A, double[,] B, double[,] result)
@@ -75,10 +114,29 @@ internal class Program
     }
 
 
-    public static void multiplyArrays( double[,] A,  double[,] B,  double[,] result)
+    public static void multiplyParallelArraysTrans(double[,] A, double[,] B, double[,] result)
     {
 
-        for(int i = 0; i < N; i++)
+        Parallel.For(0, N, i =>
+        {
+            for (int j = 0; j < N; j++)
+            {
+                double temp = 0;
+                for (int k = 0; k < N; k++)
+                {
+                    temp += A[i, k] * B[j, k];
+                }
+                result[i, j] = temp;
+            }
+        });
+
+    }
+
+
+    public static void multiplyArrays(double[,] A, double[,] B, double[,] result)
+    {
+
+        for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
@@ -87,13 +145,32 @@ internal class Program
                 {
                     temp += A[i, k] * B[k, j];
                 }
-                result[i,j] = temp;
+                result[i, j] = temp;
             }
         }
 
     }
 
-    public static void generateArray( double[,] A,  double[,] B)
+    public static void multiplyArraysTrans(double[,] A, double[,] B, double[,] result)
+    {
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                double temp = 0;
+                for (int k = 0; k < N; k++)
+                {
+                    temp += A[i, k] * B[j, k];
+                }
+                result[i, j] = temp;
+            }
+        }
+
+    }
+
+
+    public static void generateArray(double[,] A, double[,] B)
     {
         Random random = new Random(Seed);
 
@@ -108,14 +185,29 @@ internal class Program
     }
 
 
-    public static void displayArray( double[,] arrayDisplay)
+    public static void generateArrayT(double[,] A, double[,] B)
+    {
+        Random random = new Random(Seed);
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                A[i, j] = random.Next();
+                B[j, i] = random.Next();
+            }
+        }
+    }
+
+
+    public static void displayArray(double[,] arrayDisplay)
     {
 
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
-                Console.WriteLine($"Row:{i}, Col:{j} > {arrayDisplay[i, j]}");                
+                Console.WriteLine($"Row:{i}, Col:{j} > {arrayDisplay[i, j]}");
             }
         }
     }
@@ -124,9 +216,9 @@ internal class Program
     public static void displayArrayLastNums(double[,] arrayDisplay, int lastNums)
     {
 
-        for (int i = N-1; i < N; i++)
+        for (int i = N - 1; i < N; i++)
         {
-            for (int j = N-lastNums; j < N; j++)
+            for (int j = N - lastNums; j < N; j++)
             {
                 Console.WriteLine($"Row:{i}, Col:{j} > {arrayDisplay[i, j]}");
             }
